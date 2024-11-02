9f336e4f40216fb4ec14c1ea4a49969baebf4cd6
Damego <damego.dev@gmail.com>
2024-11-02 23:43:49 +0500

    chore: add check for optional building value

--- src/features/audience/service.py
+++ src/features/audience/service.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from ...core.repository import MongoDBRepository
 
 
@@ -11,5 +12,5 @@ class AudienceService:
     async def get_audience_by_number(self, number: str):
         return await self.repository.get_one_by_filter({"number": number})
 
-    async def search_audience(self, number: str, building: str):
-        return await self.repository.search_by_field("number", number, filter={"building": building})
+    async def search_audience(self, number: str, building: Optional[str] = None):
+        return await self.repository.search_by_field("number", number, filter={"building": building} if building is not None else None)
