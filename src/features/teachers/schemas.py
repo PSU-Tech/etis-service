c06bce6f8b87c38b65c3581d867761c192f49406
Damego <damego.dev@gmail.com>
2024-11-20 19:21:15 +0500

    feat!: remove info about teachers

--- src/features/teachers/schemas.py
+++ src/features/teachers/schemas.py
@@ -2,24 +2,9 @@ from typing import Optional
 
 from pydantic import BaseModel, Field, ConfigDict
 
-from src.features.departments.schemas import Department
-
-
-class TeacherETISData(BaseModel):
-    image_url: str
-    position: str
-    departments: list[Department]
-
-
-class TeacherContacts(BaseModel):
-    emails: Optional[list[str]] = Field(default=None)
-    phones: Optional[list[str]] = Field(default=None)
-
 
 class TeacherPSUData(BaseModel):
     page_url: str
-    image_url: str
-    contacts: TeacherContacts
 
 
 class Teacher(BaseModel):
@@ -27,5 +12,4 @@ class Teacher(BaseModel):
 
     id: int = Field(alias="_id")
     name: str
-    etis: Optional[TeacherETISData] = Field(default=None)
     psu: Optional[TeacherPSUData] = Field(default=None)
